ed6f2fc812045011a1bfef8ce5cc8dca6561eae7
DalyahAljamal <dalyah_se@outlook.com>
2019-04-24 22:13:31 +0300

    Add, Edit, Update and Delete

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from models import Base, User, Category, Item
-from flask import Flask, jsonify, request, url_for, abort, g
+from flask import Flask, jsonify, request, url_for, abort, g, render_template, redirect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -31,7 +31,7 @@ def verify_password(username, password):
 		return True
 
 
-@app.route('/users', methods = ['POST']) # not working
+@app.route('/users', methods = ['POST'])
 def new_user():
 	username = request.args.get('username')
 	password = request.args.get('password')
@@ -49,6 +49,7 @@ def new_user():
 	session.add(user)
 	session.commit()
 	return jsonify({ 'username': user.username }), 201
+
 @app.route('/users/<int:id>')
 def get_user(id):
 	user = session.query(User).filter_by(id=id).one()
@@ -58,29 +59,27 @@ def get_user(id):
 
 # Get Requests
 @app.route('/')
+@app.route('/catalog')
 # return all the categories and latest added items for each category
 def get_all_category_latestItem():
 	categories = session.query(Category).all()
-	return jsonify(categories = [cat.serialize for cat in categories])
+	items = session.query(Item).order_by(Item.id.desc())
+	return render_template('catalog.html', categories=categories, items=items )
 
-@app.route('/catalog/<string:cat>/items')
+@app.route('/catalog/<int:cat>/items')
 # returns the items of the specified category
 def showAllItems(cat):
-	category = session.query(Category).filter_by(name=cat).first()
-	cat_id = category.id
-	items = session.query(Item).filter_by(cat_id=cat_id)
-	# render html template by sending items and category
+	items = session.query(Item).filter_by(cat_id=cat)
+	cat = session.query(Category).filter_by(id=cat).first()
+	# return jsonify(items = [item.serialize for item in items])
+	return render_template('items.html', items=items , cat= cat.name)
 
-	return jsonify(items = [item.serialize for item in items])
-
-@app.route('/catalog/<string:cat>/<string:item>')
+@app.route('/catalog/<int:cat>/<string:item>')
 # returns des of specified item
-def showItemDes(cat, item):
-	category = session.query(Category).filter_by(name=cat).first()
-	item = session.query(Item).filter_by(title=item, cat_id= category.id).first()
+def showItemDes(item, cat):
+	item = session.query(Item).filter_by(title=item, cat_id= cat).first()
 	item_des = item.description
-	# render html template by sending item_des
-	return jsonify(item.description)
+	return render_template('item.html', item =item)
 
 @app.route('/catalog.json')
 # returns all categories in catalog
@@ -97,52 +96,61 @@ def showJSON():
 	return jsonify(categories = [category.serialize for category in categories])
 
 # PUT, DELETE, POST Requests
-@app.route('/catalog/<string:cat>/<string:item>/edit', methods = ['PUT'])
+@app.route('/catalog/<int:cat>/<int:item>/edit', methods = ['POST', 'GET'])
 # edits the specified item
 def editItem(cat, item):
-	category = session.query(Category).filter_by(name=cat).first()
-	item = session.query(Item).filter_by(title=item, cat_id= category.id).first()
-	title = request.args.get('title')
-	description = request.args.get('des')
-	if title:
-		item.title = title
-	if description:
-		item.description = description
-	session.commit()
-	return jsonify(item.serialize)
+	category = session.query(Category).filter_by(id=cat).first()
+	categories = session.query(Category).all()
+	item = session.query(Item).filter_by(id=item, cat_id= cat).first()
+	# title = request.args.get('title')
+	# description = request.args.get('des')
 
-@app.route('/catalog/<string:cat>/<string:item>/delete', methods = ['DELETE'])
+	if request.method == 'GET':
+		 return render_template('editItem.html', categories = categories,
+		                         item = item)
+	else:
+		if request.form['itemTitle']:
+			 item.title = request.form['itemTitle']
+		if request.form['itemDes']:
+			 item.description = request.form['itemDes']
+		if request.form['category']:
+			 item.cat_id = request.form['category']
+		session.commit()
+		# return "item updated"
+		return redirect(url_for('showItemDes', item = item.id,
+		                        cat = item.cat_id))
+
+@app.route('/catalog/<int:cat>/<int:item>/delete', methods = ['GET', 'DELETE'])
 # delete the specified item
 def deleteItem(cat, item):
-	category = session.query(Category).filter_by(name=cat).first()
-	item = session.query(Item).filter_by(title=item, cat_id= category.id).first()
-	session.delete(item)
-  	session.commit()
-  	return "Item Deleted"
+	category = session.query(Category).filter_by(id=cat).first()
+	currentItem = session.query(Item).filter_by(id=item, cat_id= cat).first()
+	if request.method == 'GET':
+		return render_template('deleteItem.html')
+	else:
+		session.delete(currentItem)
+	  	session.commit()
+	  	return "Item Deleted"
 
-@app.route('/additem', methods = ['GET', 'POST'])
+@app.route('/addItem', methods = ['GET', 'POST'])
 # add the specified item
 def addItem():
+	categories = session.query(Category).all()
 	if request.method == 'GET':
-	  	# render html template to add items
-		print "go to html tempalte"
-	elif request.method == 'POST':
-		des = request.args.get('description','')
-		title = request.args.get('title','')
-		cat_id = request.args.get('cat_id','')
-		print des
-		print title
-		print cat_id
-		# item = session.query(Item).filter_by(title = title).first()
-		# if item is not None:
-		# 	print "existing item"
-		# 	return jsonify({'message':'item already exists'})
-		item = Item(description = des, title= title, cat_id = cat_id)
+		 return render_template('addItem.html', categories = categories)
+	else:
+		if request.form['itemTitle']:
+			 title = request.form['itemTitle']
+		if request.form['itemDes']:
+			 description = request.form['itemDes']
+		if request.form['category']:
+			 cat_id = request.form['category']
+		item = Item(description = description, title= title, cat_id = cat_id)
 		session.add(item)
 		session.commit()
-		return jsonify({ 'id': item.id, 'description' : item.description,
-		'title': item.title, 'cat ID': item.cat_id })
-		return jsonify('Adding %s', item)
+		return "item added"
+		# return redirect(url_for('showItemDes', item = item.id,
+		#                         cat = item.cat_id))
 
 @app.route('/addcat', methods= ['GET'])
 # add cat
