a6174dd7c275ead6a1d1af1db9f34b5946daa1ae
DalyahAljamal <dalyah_se@outlook.com>
2019-04-26 21:39:45 +0300

    Bug fix and updates

--- views.py
+++ views.py
@@ -1,25 +1,28 @@
 from models import Base, User, Category, Item
-from flask import Flask, jsonify, request, url_for, abort, g, render_template, redirect
+from flask import Flask, jsonify, request, url_for, abort, g, render_template, session, redirect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from flask_httpauth import HTTPBasicAuth
+import random
+import string
 
 auth = HTTPBasicAuth()
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('sqlite:///catalog.db',
+                        connect_args={'check_same_thread': False})
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+db_session = DBSession()
 app = Flask(__name__)
 
 
 @auth.verify_password
 def verify_password(username, password):
 	print "Looking for user %s" % username
-	user = session.query(User).filter_by(username = username).first()
+	user = db_session.query(User).filter_by(username = username).first()
 	if not user:
 		print "User not found"
 		return False
@@ -39,20 +42,20 @@ def new_user():
 		print "missing arguments"
 		abort(400)
 
-	user = session.query(User).filter_by(username=username).first()
+	user = db_session.query(User).filter_by(username=username).first()
 	if user is not None:
 		print "existing user"
 		return jsonify({'message':'user already exists'}), 200
 
 	user = User(username = username)
 	user.hash_password(password)
-	session.add(user)
-	session.commit()
+	db_session.add(user)
+	db_session.commit()
 	return jsonify({ 'username': user.username }), 201
 
 @app.route('/users/<int:id>')
 def get_user(id):
-	user = session.query(User).filter_by(id=id).one()
+	user = db_session.query(User).filter_by(id=id).one()
 	if not user:
 		abort(400)
 	return jsonify({'username': user.username})
@@ -62,32 +65,30 @@ def get_user(id):
 @app.route('/catalog')
 # return all the categories and latest added items for each category
 def get_all_category_latestItem():
-	categories = session.query(Category).all()
-	items = session.query(Item).order_by(Item.id.desc())
+	categories = db_session.query(Category).all()
+	items = db_session.query(Item).order_by(Item.id.desc())
 	return render_template('catalog.html', categories=categories, items=items )
 
 @app.route('/catalog/<int:cat>/items')
 # returns the items of the specified category
 def showAllItems(cat):
-	items = session.query(Item).filter_by(cat_id=cat)
-	cat = session.query(Category).filter_by(id=cat).first()
-	# return jsonify(items = [item.serialize for item in items])
+	items = db_session.query(Item).filter_by(cat_id=cat)
+	cat = db_session.query(Category).filter_by(id=cat).first()
 	return render_template('items.html', items=items , cat= cat.name)
 
-@app.route('/catalog/<int:cat>/<string:item>')
+@app.route('/catalog/<int:cat>/<int:item>')
 # returns des of specified item
 def showItemDes(item, cat):
-	item = session.query(Item).filter_by(title=item, cat_id= cat).first()
-	item_des = item.description
+	item = db_session.query(Item).filter_by(id=item, cat_id= cat).first()
 	return render_template('item.html', item =item)
 
 @app.route('/catalog.json')
 # returns all categories in catalog
 def showJSON():
-	categories = session.query(Category).all()
+	categories = db_session.query(Category).all()
 
 	# for cat in categories:
-	# 	items = session.query(Item).filter_by( cat_id= cat.id)
+	# 	items = db_session.query(Item).filter_by( cat_id= cat.id)
 	# 	items_cat = []
 	# 	for item in items:
 	# 		items_cat.append(item.serialize)
@@ -97,79 +98,115 @@ def showJSON():
 
 # PUT, DELETE, POST Requests
 @app.route('/catalog/<int:cat>/<int:item>/edit', methods = ['POST', 'GET'])
+@auth.login_required
 # edits the specified item
 def editItem(cat, item):
-	category = session.query(Category).filter_by(id=cat).first()
-	categories = session.query(Category).all()
-	item = session.query(Item).filter_by(id=item, cat_id= cat).first()
-	# title = request.args.get('title')
-	# description = request.args.get('des')
+	if 'logged_in' in session:
+		category = db_session.query(Category).filter_by(id=cat).first()
+		categories = db_session.query(Category).all()
+		item = db_session.query(Item).filter_by(id=item, cat_id= cat).first()
 
-	if request.method == 'GET':
-		 return render_template('editItem.html', categories = categories,
-		                         item = item)
+		if request.method == 'GET':
+			 return render_template('editItem.html', categories = categories,
+			                         item = item)
+		else:
+			if request.form['itemTitle']:
+				 item.title = request.form['itemTitle']
+			if request.form['itemDes']:
+				 item.description = request.form['itemDes']
+			if request.form['category']:
+				 item.cat_id = request.form['category']
+			db_session.commit()
+			# return "item updated"
+			return redirect(url_for('showItemDes', item = item.id,
+			                        cat = item.cat_id))
 	else:
-		if request.form['itemTitle']:
-			 item.title = request.form['itemTitle']
-		if request.form['itemDes']:
-			 item.description = request.form['itemDes']
-		if request.form['category']:
-			 item.cat_id = request.form['category']
-		session.commit()
-		# return "item updated"
-		return redirect(url_for('showItemDes', item = item.id,
-		                        cat = item.cat_id))
+		return redirect(url_for('login'))
 
-@app.route('/catalog/<int:cat>/<int:item>/delete', methods = ['GET', 'DELETE'])
+
+@app.route('/catalog/<int:cat>/<int:item>/delete', methods = ['GET', 'DELETE', 'POST'])
+@auth.login_required
 # delete the specified item
 def deleteItem(cat, item):
-	category = session.query(Category).filter_by(id=cat).first()
-	currentItem = session.query(Item).filter_by(id=item, cat_id= cat).first()
-	if request.method == 'GET':
-		return render_template('deleteItem.html')
+	if 'logged_in' in session:
+		category = db_session.query(Category).filter_by(id=cat).first()
+		currentItem = db_session.query(Item).filter_by(id=item, cat_id= cat).first()
+		if request.method == 'GET':
+			return render_template('deleteItem.html', item= currentItem)
+		else:
+			db_session.delete(currentItem)
+		  	db_session.commit()
+		  	print "Item Deleted"
+			return redirect(url_for('get_all_category_latestItem'))
 	else:
-		session.delete(currentItem)
-	  	session.commit()
-	  	return "Item Deleted"
+		return redirect(url_for('login'))
+
 
 @app.route('/addItem', methods = ['GET', 'POST'])
+@auth.login_required
 # add the specified item
 def addItem():
-	categories = session.query(Category).all()
-	if request.method == 'GET':
-		 return render_template('addItem.html', categories = categories)
+	if 'logged_in' in session:
+		categories = db_session.query(Category).all()
+		if request.method == 'GET':
+			 return render_template('addItem.html', categories = categories)
+		else:
+			if request.form['itemTitle']:
+				 title = request.form['itemTitle']
+			if request.form['itemDes']:
+				 description = request.form['itemDes']
+			if request.form['category']:
+				 cat_id = request.form['category']
+			item = Item(description = description, title= title, cat_id = cat_id)
+			db_session.add(item)
+			db_session.commit()
+			print "item added"
+			return redirect(url_for('get_all_category_latestItem'))
 	else:
-		if request.form['itemTitle']:
-			 title = request.form['itemTitle']
-		if request.form['itemDes']:
-			 description = request.form['itemDes']
-		if request.form['category']:
-			 cat_id = request.form['category']
-		item = Item(description = description, title= title, cat_id = cat_id)
-		session.add(item)
-		session.commit()
-		return "item added"
-		# return redirect(url_for('showItemDes', item = item.id,
-		#                         cat = item.cat_id))
+		return redirect(url_for('login'))
 
 @app.route('/addcat', methods= ['GET'])
-# add cat
+@auth.login_required
+# add category
 def addCategory():
-	id = request.json.get('id')
-	name = request.json.get('name')
-	cat = session.query(Category).filter_by(id = id).first()
-	if cat is not None:
-		print "existing category"
-		return jsonify({'message':'category already exists'}), 200
-	cat = Category(id = id, name = name)
-	session.add(cat)
-	session.commit()
-	return jsonify({ 'id': cat.id, 'name' : cat.name }), 201
+	if 'logged_in' in session:
+		id = request.json.get('id')
+		name = request.json.get('name')
+		cat = db_session.query(Category).filter_by(id = id).first()
+		if cat is not None:
+			print "existing category"
+			return jsonify({'message':'category already exists'}), 200
+		cat = Category(id = id, name = name)
+		db_session.add(cat)
+		db_session.commit()
+		return jsonify({ 'id': cat.id, 'name' : cat.name }), 201
+	else:
+		return redirect(url_for('login'))
 
-
-
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		if verify_password(username, password):
+			session['logged_in'] = True
+			print "Successful Login"
+			return redirect(url_for('get_all_category_latestItem'))
+		else:
+			print "Sorry Wrong Credintials"
+			return redirect(url_for('login'))
+@app.route('/logout', methods = ['GET'])
+def logout():
+	if 'logged_in' in session:
+		del session['logged_in']
+		# session['logged_in'] = False
+		print "logout suceessfully"
+		return redirect(url_for('get_all_category_latestItem'))
 
 
 if __name__ == '__main__':
 	app.debug = True
+	app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 	app.run(host='0.0.0.0', port=5000)
